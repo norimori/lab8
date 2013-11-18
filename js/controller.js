7272425c70388f14595340ea0261224c13725e69
Midori Mori <zaseaweed@gmail.com>
2013-11-18 01:42:47 -0800

    required steps completed

--- js/controller.js
+++ js/controller.js
@@ -30,5 +30,26 @@ $(function(){
 	//refresh to get movies from server
 	moviesModel.refresh();
 
+
+	//when the movies view triggers 'addToCart'
+	//add a new item to the cart, using the supplied
+	//movieID and format
+	moviesView.on('addToCart', function(data){
+	    var movie = moviesModel.getItem(data.movieID);
+	    if (!movie)
+	        throw 'Invalid movie ID "' + movieID + '"!'; 
+
+	    cartModel.addItem({
+	        id: movie.id,
+	        title: movie.title,
+	        format: data.format,
+	        formatLabel: formatLabels[data.format],
+	        price: movie.prices[data.format]
+	    });
+	}); //addToCart event
+
+	$('.place-order').click(function() {
+		console.log(cartModel.toJSON());
+	});
 }); //doc ready()
 
